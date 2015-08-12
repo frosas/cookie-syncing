bac95ced2588177b529dbe3d49dfbfed23f4a70e
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-08-12 13:31:16 +0100

    WIP

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 var express = require('express');
 
-express().use(express.static('public/site')).listen(8000);
-express().use(express.static('public/id-site')).listen(8001);
+express().
+    use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    }).
+    use(express.static('public/site')).
+    listen(8000);
+    
+express().
+    use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    }).
+    use(express.static('public/id-site')).
+    listen(8001);
